420f562d6df4665938f921c5757687bfe6aed202
Yuval Yaffe <yuvy1072@gmail.com>
2020-04-26 17:27:13 +0300

    Update image_downloader to not download same file if it is already downloaded

--- utils/image_downloader.py
+++ utils/image_downloader.py
@@ -1,7 +1,7 @@
 from fastai.vision import download_images
 import os
 
-def download_image_dataset(urls_base,dest,max_workers):
+def download_image_dataset(urls_base,dest,max_workers,force_download=False):
     files_list = []
     for (root,dirs,files) in os.walk(urls_base):
         for file in files:
@@ -9,6 +9,11 @@ def download_image_dataset(urls_base,dest,max_workers):
             dest_path_dir = os.path.join(dest,os.path.relpath(root,urls_base))
             if not os.path.exists(dest_path_dir):
                 os.makedirs(dest_path_dir)
-            print(f"Downloading {src_path}")
-            download_images(src_path,dest_path_dir,max_workers=max_workers)
+            downloaded_flag_file = os.path.join(dest_path_dir,f".downloaded_{file}")
+            if os.path.exists(downloaded_flag_file) and not force_download:
+                print(f"{src_path} already downloaded")
+            else:
+                print(f"Downloading {src_path}")
+                download_images(src_path,dest_path_dir,max_workers=max_workers)
+                open(downloaded_flag_file,'w').close()
         
